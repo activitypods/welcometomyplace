ff5a3eacafda66115075697e9bc25bbc15b47dbc
srosset81 <srosset81@gmail.com>
2022-04-27 14:28:09 +0200

    Fix redirection

--- frontend/src/pages/OffersPage.js
+++ frontend/src/pages/OffersPage.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const OffersPage = () => {
   const params = useParams();
-  window.location.href = 'https://lentraide.app/offers/' + encodeURIComponent(params.id);
+  window.location.href = 'https://lentraide.app/offers/' + encodeURIComponent(params.id) + '/show';
   return null;
 };
 
--- frontend/src/pages/RequestsPage.js
+++ frontend/src/pages/RequestsPage.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const RequestsPage = () => {
   const params = useParams();
-  window.location.href = 'https://lentraide.app/requests/' + encodeURIComponent(params.id);
+  window.location.href = 'https://lentraide.app/requests/' + encodeURIComponent(params.id) + '/show';
   return null;
 };
 
