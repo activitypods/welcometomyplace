f266f8cde9b498df144f5d9a2b554ce78fff597d
srosset81 <srosset81@gmail.com>
2022-05-06 19:45:50 +0200

    Load suggestions from attendees

--- frontend/src/commons/fields/CommentsField/CommentsField.js
+++ frontend/src/commons/fields/CommentsField/CommentsField.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useNotify, useRecordContext, useTranslate } from 'react-admin';
+import { useGetIdentity, useNotify, useRecordContext, useTranslate } from 'react-admin';
 import { RichTextInput, DefaultEditorOptions } from 'ra-richtext-tiptap';
 import Mention from '@tiptap/extension-mention';
-import { Box, Button, makeStyles } from '@material-ui/core';
+import {Avatar, Box, Button, Grid, makeStyles} from '@material-ui/core';
 import { Form } from 'react-final-form';
 import SendIcon from '@material-ui/icons/Send';
 import { useOutbox, useCollection, OBJECT_TYPES } from '@semapps/activitypub-components';
 import Alert from "@material-ui/lab/Alert";
-import suggestion from './suggestion'
+import useSuggestions from "./useSuggestions";
 
 const useStyles = makeStyles(theme => ({
+  avatar: {
+    width: 65,
+    height: 65,
+    marginTop: 15
+  },
   editorContent: {
     '& > div': {
       backgroundColor: 'rgba(0, 0, 0, 0.09)',
@@ -17,17 +22,20 @@ const useStyles = makeStyles(theme => ({
       borderWidth: '0px !important',
       borderRadius: 0,
       borderBottom: '1px solid #FFF',
-    },
+      minHeight: 60
+    }
   }
 }));
 
 const ContactField = ({ source, context, ...rest }) => {
   const record = useRecordContext(rest);
+  const { identity } = useGetIdentity();
   const classes = useStyles();
   const notify = useNotify();
   const outbox = useOutbox();
   const translate = useTranslate();
   const { items: contacts, loaded: contactsLoaded } = useCollection('apods:contacts');
+  const suggestion = useSuggestions();
 
   const onSubmit = async (values) => {
     console.log('values', values);
@@ -45,6 +53,9 @@ const ContactField = ({ source, context, ...rest }) => {
     // }
   };
 
+  // Don't init the comment field until the suggestions are loaded, otherwise it will not work
+  if( !suggestion.items ) return null;
+
   return (
     <Form
       onSubmit={onSubmit}
@@ -55,25 +66,37 @@ const ContactField = ({ source, context, ...rest }) => {
               <Alert severity="warning">{translate('app.helper.message_profile_show_right', { username: record?.['vcard:given-name']})}</Alert>
             </Box>
           }
-          <RichTextInput
-            source="comment"
-            label=""
-            toolbar={null}
-            fullWidth
-            classes={{ editorContent: classes.editorContent }}
-            editorOptions={{
-              ...DefaultEditorOptions,
-              extensions: [
-                ...DefaultEditorOptions.extensions,
-                Mention.configure({
-                  HTMLAttributes: {
-                    class: 'mention',
-                  },
-                  suggestion,
-                })
-              ],
-            }}
-          />
+          <Grid container>
+            <Grid item xs="1">
+              <Avatar
+                src={identity?.profileData?.['vcard:photo']}
+                alt={identity?.profileData?.['vcard:given-name']}
+                className={classes.avatar}
+              />
+
+            </Grid>
+            <Grid item xs="11">
+              <RichTextInput
+                source="comment"
+                label=""
+                toolbar={null}
+                fullWidth
+                classes={{ editorContent: classes.editorContent }}
+                editorOptions={{
+                  ...DefaultEditorOptions,
+                  extensions: [
+                    ...DefaultEditorOptions.extensions,
+                    Mention.configure({
+                      HTMLAttributes: {
+                        class: 'mention',
+                      },
+                      suggestion,
+                    })
+                  ],
+                }}
+              />
+            </Grid>
+          </Grid>
           <Box mt={1}>
             <Button type="submit" variant="contained" color="primary" endIcon={<SendIcon />} disabled={submitting}>
               {translate('app.action.send')}
---
+++ frontend/src/commons/fields/CommentsField/useSuggestions.js
@@ -0,0 +1,81 @@
+import { useMemo } from 'react';
+import { useRecordContext, useQueryWithStore } from "react-admin";
+import { ReactRenderer } from '@tiptap/react'
+import tippy from 'tippy.js'
+import MentionList from './MentionList'
+import { useCollection } from '@semapps/activitypub-components';
+
+const render = () => {
+  let component
+  let popup
+
+  return {
+    onStart: props => {
+      component = new ReactRenderer(MentionList, {
+        props,
+        editor: props.editor,
+      })
+
+      popup = tippy('body', {
+        getReferenceClientRect: props.clientRect,
+        appendTo: () => document.body,
+        content: component.element,
+        showOnCreate: true,
+        interactive: true,
+        trigger: 'manual',
+        placement: 'bottom-start',
+      })
+    },
+
+    onUpdate(props) {
+      component.updateProps(props)
+
+      popup[0].setProps({
+        getReferenceClientRect: props.clientRect,
+      })
+    },
+
+    onKeyDown(props) {
+      if (props.event.key === 'Escape') {
+        popup[0].hide()
+
+        return true
+      }
+
+      return component.ref?.onKeyDown(props)
+    },
+
+    onExit() {
+      popup[0].destroy()
+      component.destroy()
+    },
+  }
+};
+
+const useSuggestions = () => {
+  const record = useRecordContext()
+  const { items: attendees, loaded: attendeesLoaded } = useCollection(record['apods:attendees']);
+
+  const { data: profiles, loaded: profilesLoaded } = useQueryWithStore({
+    type: 'getList',
+    resource: 'Profile'
+  });
+
+  const items = useMemo(() => {
+    if( attendeesLoaded && profilesLoaded ) {
+      return ({ query }) => {
+        return profiles
+          .filter(profile => attendees.includes(profile.describes))
+          .map(profile => profile['vcard:given-name'])
+          .filter(name => name.toLowerCase().startsWith(query.toLowerCase()))
+          .slice(0, 5)
+      }
+    }
+  }, [attendeesLoaded, profilesLoaded, attendees, profiles]);
+
+  return ({
+    items, render
+  });
+};
+
+export default useSuggestions;
