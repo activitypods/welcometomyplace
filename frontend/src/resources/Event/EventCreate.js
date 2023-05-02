d3f95641e6e245824b24005dd9787598432d1389
srosset81 <srosset81@gmail.com>
2023-05-02 16:58:10 +0200

    Remove warning if no address exist when creating event

--- frontend/src/resources/Event/EventCreate.js
+++ frontend/src/resources/Event/EventCreate.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { CreateBase, useQueryWithStore, Loading, useTranslate } from 'react-admin';
+import { CreateBase, useTranslate } from 'react-admin';
 import EventForm from './EventForm';
-import Alert from '@material-ui/lab/Alert';
 import CreatePage from '../../layout/CreatePage';
 
 const EventCreate = (props) => {
-  const { loading, data: locations } = useQueryWithStore({ type: 'getList', resource: 'Location' });
   const translate = useTranslate();
-  return loading ? (
-    <Loading />
-  ) : locations.length === 0 ? (
-    <Alert severity="error">{translate('app.helper.no_address')}</Alert>
-  ) : (
+  return (
     <CreateBase {...props}>
       <CreatePage title={translate('app.action.create_event')}>
         <EventForm />
