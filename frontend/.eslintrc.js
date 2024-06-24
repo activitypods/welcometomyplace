a3a966a5f799cca2787c17e198a62dbdcf8d246e
srosset81 <srosset81@gmail.com>
2024-06-24 17:49:06 +0200

    Initial work

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+export default {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+  },
+};
---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: { ".js": "jsx" },
+  },
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    host: true,
+  },
+  base: "./",
+});
