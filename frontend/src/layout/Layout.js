6956f1711265068509f7c59570d5888fdc07310b
srosset81 <srosset81@gmail.com>
2024-09-11 18:09:15 +0200

    Upgrade packages

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -3,13 +3,21 @@ import { Box } from '@mui/material';
 import { BackgroundChecks } from '@activitypods/react';
 import AppBar from './AppBar';
 import ScrollToTop from './ScrollToTop';
+import { useGetIdentity } from 'react-admin';
 
-const Layout = ({ children, title }) => (
-  <BackgroundChecks>
-    <ScrollToTop />
-    <AppBar title={title} />
-    <Box>{children}</Box>
-  </BackgroundChecks>
-);
+const Layout = ({ children, title }) => {
+  const { data: identity, isLoading } = useGetIdentity();
+  if (isLoading) return null;
+  return (
+    <BackgroundChecks
+      clientId={process.env.REACT_APP_BACKEND_CLIENT_ID}
+      listeningTo={[identity?.webIdData.inbox, identity?.webIdData.outbox]}
+    >
+      <ScrollToTop />
+      <AppBar title={title} />
+      <Box>{children}</Box>
+    </BackgroundChecks>
+  );
+};
 
 export default Layout;
