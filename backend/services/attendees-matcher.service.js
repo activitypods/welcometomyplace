95195ea118f8beb66ddc67a93bc0b4cffab5f543
srosset81 <srosset81@gmail.com>
2024-11-13 11:41:51 +0100

    Fix offer contact is not sent by the right actor

--- backend/services/attendees-matcher.service.js
+++ backend/services/attendees-matcher.service.js
@@ -82,7 +82,7 @@ module.exports = {
                 target: otherAttendeeUri,
                 to: otherAttendeeUri
               },
-              actorUri: otherAttendeeUri
+              actorUri: attendees[attendeeUri].id
             });
           }
         }
