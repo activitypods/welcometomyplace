db9ccfd355d2d99671aaa1dd9eb1de0037155455
srosset81 <srosset81@gmail.com>
2025-10-27 11:59:51 +0100

    Clean up actions to remove notifications

--- backend/services/cleanup.service.js
+++ backend/services/cleanup.service.js
@@ -1,8 +1,105 @@
+const urlJoin = require('url-join');
 const { arrayOf, delay } = require('@semapps/ldp');
+const CONFIG = require('../config/config');
 
 module.exports = {
   name: 'cleanup',
   actions: {
+    async removeAllNotifications(ctx) {
+      // First delete the ACL of the notifications
+      await ctx.call('triplestore.update', {
+        query: `
+         DELETE {
+  		     GRAPH <http://semapps.org/webacl> {
+              ?aclUri <http://www.w3.org/ns/auth/acl#accessTo> ?activityUri .
+              ?aclUri ?p ?o .
+            }
+          }
+          WHERE {
+            ?activityUri a <http://activitypods.org/ns/core#Notification> .
+            GRAPH <http://semapps.org/webacl> {
+              ?aclUri <http://www.w3.org/ns/auth/acl#accessTo> ?activityUri .
+              ?aclUri ?p ?o .
+            }
+          }
+        `,
+        webId: 'system'
+      });
+
+      // Then the links to the notifications
+      await ctx.call('triplestore.update', {
+        query: `
+          DELETE {
+            ?containerUri <http://www.w3.org/ns/ldp#contains> ?activityUri .
+            ?collectionUri <https://www.w3.org/ns/activitystreams#items> ?activityUri .
+          }
+		      WHERE {
+            ?activityUri a <http://activitypods.org/ns/core#Notification> .
+            OPTIONAL { ?containerUri <http://www.w3.org/ns/ldp#contains> ?activityUri . }
+            OPTIONAL { ?collectionUri <https://www.w3.org/ns/activitystreams#items> ?activityUri . }
+          }
+        `,
+        webId: 'system'
+      });
+
+      // Then the notifications themselves
+      await ctx.call('triplestore.update', {
+        query: `
+          DELETE
+		      WHERE {
+            ?activityUri a <http://activitypods.org/ns/core#Notification> .
+            ?activityUri ?p ?o .
+          }
+        `,
+        webId: 'system'
+      });
+
+      // Then the orphan blank nodes
+      await ctx.call('triplestore.update', {
+        query: `
+          DELETE {
+            ?s ?p ?o .
+          }
+          WHERE {
+            ?s ?p ?o .
+            FILTER(isBLANK(?s))
+            FILTER(NOT EXISTS {?parentS ?parentP ?s})
+          }
+        `,
+        webId: 'system'
+      });
+    },
+    async removeOldNotifications(ctx) {
+      const limitDate = new Date();
+      limitDate.setMonth(limitDate.getMonth() - 3);
+
+      const activitiesUris = await ctx.call('ldp.container.getUris', {
+        containerUri: urlJoin(CONFIG.HOME_URL, 'as/activity')
+      });
+
+      for (const activityUri of activitiesUris) {
+        const activity = await ctx.call('activitypub.activity.get', { resourceUri: activityUri, webId: 'system' });
+
+        const activityDate = new Date(activity['dc:created']);
+        const appOutbox = urlJoin(CONFIG.HOME_URL, 'app/outbox');
+
+        if (arrayOf(activity.type).includes('apods:Notification') && activityDate < limitDate) {
+          this.logger.info(`Deleting notification ${activity.name}...`);
+
+          await ctx.call('ldp.resource.delete', { resourceUri: activityUri, webId: 'system' });
+
+          await ctx.call('triplestore.update', {
+            query: `
+              DELETE
+              WHERE {
+                <${appOutbox}> <https://www.w3.org/ns/activitystreams#items> <${activityUri}>
+              }
+            `,
+            webId: 'system'
+          });
+        }
+      }
+    },
     async removeOldEvents(ctx) {
       const actorsUris = ctx.params.actorUri
         ? [ctx.params.actorUri]
