7ff4728dc747bf4607002d46bcaa1b8b9617a060
srosset81 <srosset81@gmail.com>
2025-10-27 12:00:12 +0100

    Skip delete orphan blank nodes calls to improve performances

--- backend/moleculer.config.js
+++ backend/moleculer.config.js
@@ -15,10 +15,22 @@ const cacherConfig = CONFIG.REDIS_CACHE_URL
     }
   : undefined;
 
+const SkipOrphanBlankNodesCleanupMiddleware = () => ({
+  name: 'SkipOrphanBlankNodesCleanupMiddleware',
+  localAction: (next, action) => {
+    if (action.name === 'triplestore.deleteOrphanBlankNodes') {
+      return async ctx => {};
+    } else {
+      return next;
+    }
+  }
+});
+
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
   middlewares: [
+    SkipOrphanBlankNodesCleanupMiddleware(),
     CacherMiddleware(cacherConfig), // Set the cacher before the WebAcl middleware
     WebAclMiddleware({ baseUrl: CONFIG.HOME_URL })
   ],
